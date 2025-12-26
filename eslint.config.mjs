671c205cd797562475d373320b1584435853f66b
Ally Summers <ally@summers.pw>
2025-12-26 23:54:52 +1100

    formatting settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,58 +1,53 @@
-import nx from "@nx/eslint-plugin";
-import stylistic from "@stylistic/eslint-plugin";
-import perfectionist from "eslint-plugin-perfectionist";
+import nx from '@nx/eslint-plugin';
+import stylistic from '@stylistic/eslint-plugin';
+import perfectionist from 'eslint-plugin-perfectionist';
 
 export default [
-  ...nx.configs["flat/base"],
-  ...nx.configs["flat/typescript"],
-  ...nx.configs["flat/javascript"],
-  {
-    ignores: [
-      "**/dist",
-      "**/out-tsc",
-      "**/test-output",
-      "**/vite.config.*.timestamp*",
-    ],
-  },
-  {
-    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx"],
-    rules: {
-      "@nx/enforce-module-boundaries": [
-        "error",
-        {
-          enforceBuildableLibDependency: true,
-          allow: ["^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$"],
-          depConstraints: [
-            {
-              sourceTag: "*",
-              onlyDependOnLibsWithTags: ["*"],
-            },
-          ],
-        },
-      ],
-    },
-  },
-  stylistic.configs.customize({
-    // the following options are the default values
-    indent: 4,
-    quotes: "single",
-    semi: true,
-    jsx: true,
-  }),
-  perfectionist.configs["recommended-natural"],
-  {
-    files: [
-      "**/*.ts",
-      "**/*.tsx",
-      "**/*.cts",
-      "**/*.mts",
-      "**/*.js",
-      "**/*.jsx",
-      "**/*.cjs",
-      "**/*.mjs",
-    ],
-    plugins: {},
-    // Override or add rules here
-    rules: {},
-  },
+	...nx.configs['flat/base'],
+	...nx.configs['flat/typescript'],
+	...nx.configs['flat/javascript'],
+	{
+		ignores: ['**/dist', '**/out-tsc', '**/test-output', '**/vite.config.*.timestamp*'],
+	},
+	{
+		files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+		rules: {
+			'@nx/enforce-module-boundaries': [
+				'error',
+				{
+					allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?[jt]s$'],
+					depConstraints: [
+						{
+							onlyDependOnLibsWithTags: ['*'],
+							sourceTag: '*',
+						},
+					],
+					enforceBuildableLibDependency: true,
+				},
+			],
+		},
+	},
+	stylistic.configs.customize({
+		// the following options are the default values
+		indent: 'tab',
+		jsx: true,
+		quotes: 'single',
+		semi: true,
+	}),
+	perfectionist.configs['recommended-natural'],
+	{
+		files: [
+			'**/*.ts',
+			'**/*.tsx',
+			'**/*.cts',
+			'**/*.mts',
+			'**/*.js',
+			'**/*.jsx',
+			'**/*.cjs',
+			'**/*.mjs',
+		],
+		plugins: {},
+		// Override or add rules here
+		rules: {},
+	},
 ];
