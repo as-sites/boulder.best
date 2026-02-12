a89840a43e378c4432391f16072ffc9147b71449
Ally <9024018+AllySummers@users.noreply.github.com>
2026-02-12 10:45:54 +0000

    single nextjs app

--- apps/boulder.best/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Index() {
-	/*
-	 * Replace the elements below with your own.
-	 *
-	 * Note: The corresponding styles are in the ./index.tailwind file.
-	 */
-	return <h1>bouldering tracker at boulder.best coming soon yo</h1>;
-}
